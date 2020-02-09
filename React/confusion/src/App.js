b8162216f9922191b149d49228572181ee295118
jinwei zhang <jinwei zhang>
2020-02-09 14:04:43 +0000

    week1 lec

--- React/confusion/src/App.js
+++ React/confusion/src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import {DISHES} from './shared/dishes'
 
-function App() {
+function dishes(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render(){
   return (
     <div>
       <Navbar dark color = "primary">
@@ -12,9 +25,10 @@ function App() {
            <NavbarBrand href="/">Ristore ccon Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={this.state.dishes} />
     </div>
   );
+}
 }
 
 export default App;
